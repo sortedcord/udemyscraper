c9641532b580058510198c1326bd79c891dbd7dd
sortedcord <divyashgupta2005@gmail.com>
2021-08-04 16:16:21 +0530

    Optimizing code

--- no_selenium.py
+++ no_selenium.py
@@ -10,6 +10,9 @@ page = requests.get(url)
 content = page.content
 soup = BeautifulSoup(content, "html.parser")
 
+# with open('demo.html','w', encoding='utf-8') as file:
+#     file.write(str(soup.prettify()))
+
 # breadcrumbs = soup.find("div", class_="topic-menu udlite-breadcrumb")
 # raw_tags = breadcrumbs.find_all("a", class_="udlite-heading-sm")
 # tags = []
@@ -24,8 +27,15 @@ soup = BeautifulSoup(content, "html.parser")
 # course_language = soup.find("div", class_="clp-lead__element-item clp-lead__locale").text
 # print(course_language)
 
-objectives = []
-raw_obj = soup.find_all("span", class_="what-you-will-learn--objective-item--ECarc")
-for objective in raw_obj:
-    objectives.append(objective.text)
+
+## This does not show all of the sections, so I will be using selenium after all, firstly, to move the mouse to the get more sections one and then click on it. Which will be done in a later commit..
+sections = soup.find_all("div", class_="section--panel--1tqxC panel--panel--3NYBX")
+
+for section in sections:
+    name = section.find("span", class_="section--section-title--8blTh")
+    lessons = section.find_all("div", class_="udlite-block-list-item-content")
+    for lesson in lessons:
+        name = lesson.find("span")
+        print(name.text)
+        
 
