dd5ab5ad2043613c23f283f9091a03c131fcd7fe
sortedcord <divyashgupta2005@gmail.com>
2021-08-05 12:25:43 +0530

    Improved readability

--- fetch.py
+++ fetch.py
@@ -1,16 +1,42 @@
+import time
+import json
 from bs4 import BeautifulSoup
+
+#Selenium Libraries
 from selenium import webdriver   # for webdriver
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
-import time
 from selenium.common.exceptions import WebDriverException
-import json
+
 
 
 class Course():
     def __init__(self, query):
         self.query = query
 
+
     def fetchCourse(self):
+            # Course class will contain an array with section classes
+        class Section():
+            def __init__(self, html):
+                #Section class will contain an array with lesson classes
+                class Lesson():
+                    def __init__(self, lesson_html):
+                        self.lesson_html = lesson_html
+                        self.name = lesson_html.find("span").text
+                        
+                self.html = BeautifulSoup(html, "lxml")
+                self.name = self.html.find("span", class_="section--section-title--8blTh").text
+                self.lesson_blocks = self.html.find_all("div", class_="udlite-block-list-item-content")
+
+                self.lessons = []
+                for lesson in self.lesson_blocks:
+                    self.lessons.append(Lesson(lesson))
+                
+                self.no_of_lessons = len(self.lessons)
+                
+                other_info_block = self.html.find_all("span", class_="udlite-text-sm section--hidden-on-mobile--171Q9 section--section-content--9kwnY")
+
+        #Get the url of the search query
         url = "https://www.udemy.com/courses/search/?src=ukw&q=" + self.query
 
         # Browser Options
@@ -25,57 +51,49 @@ class Course():
         content = browser.page_source
         searchpage = BeautifulSoup(content, "lxml")
 
-
+        # Get coursepage Link
         links = []
         for a in searchpage.find_all('a', class_='udlite-custom-focus-visible browse-course-card--link--3KIkQ', href=True): 
             if a.text: 
                 links.append(a['href'])
-        self.link = 'https://udemy.com' + (links[0])
+    
+        other_info = searchpage.find_all('span', class_="course-card--row--1OMjg")
+        truncated_info = other_info[0:3]
 
+        # Get course basic metadata
+        self.link = 'https://udemy.com' + (links[0])
         self.title = searchpage.find('div', class_="udlite-focus-visible-target udlite-heading-md course-card--course-title--2f7tE").text
         self.headline = searchpage.find('p', class_="udlite-text-sm course-card--course-headline--yIrRk").text
         self.instructor = searchpage.find('div', class_="udlite-text-xs course-card--instructor-list--lIA4f").text
         self.rating = searchpage.find('span', class_="udlite-heading-sm star-rating--rating-number--3lVe8").text
-
-        other_info = searchpage.find_all('span', class_="course-card--row--1OMjg")
-        truncated_info = other_info[0:3]
-
         self.duration = truncated_info[0].text
         self.no_of_lectures = truncated_info[1].text
 
-        class Section():
-            def __init__(self, html):
-                class Lesson():
-                    def __init__(self, lesson_html):
-                        self.lesson_html = lesson_html
-                        self.name = lesson_html.find("span").text
-                        
-                self.html = BeautifulSoup(html, "lxml")
-                self.name = self.html.find("span", class_="section--section-title--8blTh").text
-                self.lesson_blocks = self.html.find_all("div", class_="udlite-block-list-item-content")
-
-                self.lessons = []
-                for lesson in self.lesson_blocks:
-                    self.lessons.append(Lesson(lesson))
-                self.no_of_lessons = len(self.lessons)
-                
-
-                other_info_block = self.html.find_all("span", class_="udlite-text-sm section--hidden-on-mobile--171Q9 section--section-content--9kwnY")
-
-        '''
-                SCRAPE COURSE PAGE
-        '''
-
+        
+        # Scrape Information on coursepage
         url = self.link
         browser.get(url)
-        time.sleep(3)
+
+        time.sleep(3) #Wait for the browser to completely load the page. You can change this depending on your internet speed.
+
+        #Try to click on show more sections button
         try:
-            browser.execute_script("var zabutton = document.getElementsByClassName('udlite-btn udlite-btn-medium udlite-btn-secondary udlite-heading-sm curriculum--show-more--2tshH' )[0]; zabutton.click();")
+            browser.execute_script("""var zabutton = document.getElementsByClassName('udlite-btn udlite-btn-medium 
+                                                                                    udlite-btn-secondary udlite-heading-sm 
+                                                                                    curriculum--show-more--2tshH' 
+                                                                                    )[0]; zabutton.click();""")
+        # If there are less than 10 sections
         except WebDriverException:
             print("More sections button could not be found. This course has less than 10 sections then.")
+
+        # Get the html
         content = browser.page_source
         browser.close()
+
+        #Parse HTML
         coursepage = BeautifulSoup(content, "lxml")
+
+        # Get breadcrumb tags
         self.tags = []
         for tag in coursepage.find("div", class_="topic-menu udlite-breadcrumb").find_all("a", class_="udlite-heading-sm"):
             self.tags.append(tag.text)
@@ -85,10 +103,12 @@ class Course():
         self.no_of_students = coursepage.find("div", attrs={"data-purpose": "enrollment"}).text.replace("\n","").replace(" students", "")
         self.course_language = coursepage.find("div", class_="clp-lead__element-item clp-lead__locale").text.replace("\n","")
 
+        #Get the pointers present in "What you'll learn part"
         self.objectives = []
         for objective in coursepage.find_all("span", class_="what-you-will-learn--objective-item--ECarc"):
             self.objectives.append(objective.text)
 
+        # This is the sections array which will contain Section classes
         self.Sections = []
 
         string_section = []
@@ -97,18 +117,22 @@ class Course():
         for section_block in string_section:
             self.Sections.append(Section(section_block))
 
+        # Get the requirements section
         self.requirements = []
         for requirement in coursepage.find("div", class_="ud-component--course-landing-page-udlite--requirements").find_all("div", class_="udlite-block-list-item udlite-block-list-item-small udlite-block-list-item-tight udlite-block-list-item-neutral udlite-text-sm"):
             self.requirements.append(requirement.text)
 
+        # Get the long description section. Each paragraph is concatenated to the description string separated by a "\n" or a new line.
         self.description = ""
         for parahraph in coursepage.find("div", attrs={'data-purpose':'safely-set-inner-html:description:description'}).find_all("p"):
             self.description += parahraph.text + "\n"
 
+        # Get the information in the target section section
         self.target_audience = []
         for a in coursepage.find("div", attrs={'data-purpose': 'target-audience'}).find_all("li"):
             self.target_audience.append(a.text)
 
+        # Get the url of the course
         self.banner = str(coursepage.find("div", class_="intro-asset--asset--1eSsi").find("img").attrs['src']).replace("240x135","480x270")
 
 
