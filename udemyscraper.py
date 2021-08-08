42cec2f57bd78327ba7e98feb82c8efd7526fd26
sortedcord <divyashgupta2005@gmail.com>
2021-08-08 12:39:28 +0530

    :snowflake: Updated in terms of PEP8

--- udemyscraper.py
+++ udemyscraper.py
@@ -2,10 +2,11 @@ import time
 import json
 from bs4 import BeautifulSoup
 
-#Selenium Libraries
+# Selenium Libraries
 from selenium import webdriver   # for webdriver
-from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 
+# for suppressing the browser
+from selenium.webdriver.chrome.options import Options
 
 
 class UdemyCourse():
@@ -13,96 +14,117 @@ class UdemyCourse():
     with open('warning.txt') as file:
         lines = file.readlines()
         for line in lines:
-            print(line.replace("\n",""))
+            print(line.replace("\n", ""))
             time.sleep(0.4)
 
     def __init__(self, query):
         self.query = query
 
-
     def fetch_course(self):
-            # Course class will contain an array with section classes
+        # Course class will contain an array with section classes
         class Section():
             def __init__(self, html):
-                #Section class will contain an array with lesson classes
+                # Section class will contain an array with lesson classes
                 class Lesson():
                     def __init__(self, lesson_html):
                         self.lesson_html = lesson_html
                         self.name = lesson_html.find("span").text
-                        
+
                 self.html = BeautifulSoup(html, "lxml")
-                self.name = self.html.select("span[class*='section--section-title--8blTh']")[0].text
-                self.lesson_blocks = self.html.find_all("div", class_="udlite-block-list-item-content")
+                self.name = self.html.select(
+                    "span[class*='section--section-title--8blTh']")[0].text
+                self.lesson_blocks = self.html.find_all(
+                    "div", class_="udlite-block-list-item-content")
 
                 self.lessons = []
                 for lesson in self.lesson_blocks:
                     self.lessons.append(Lesson(lesson))
-                
-                self.no_of_lessons = len(self.lessons)
-                
-                other_info_block = self.html.find_all("span", class_="udlite-text-sm section--hidden-on-mobile--171Q9 section--section-content--9kwnY")
 
-        #Get the url of the search query
+                self.no_of_lessons = len(self.lessons)
+
+                other_info_block = self.html.find_all(
+                    "span", class_="udlite-text-sm section--hidden-on-mobile--171Q9 section--section-content--9kwnY")
+
+        # Get the url of the search query
         url = "https://www.udemy.com/courses/search/?src=ukw&q=" + self.query
 
         # Browser Options
         option = Options()
         option.add_argument('headless')
         option.add_experimental_option('excludeSwitches', ['enable-logging'])
-        browser = webdriver.Chrome(executable_path='chromedriver.exe', chrome_options=option)
+        browser = webdriver.Chrome(
+            executable_path='chromedriver.exe', )  # chrome_options=option
         browser.get(url)
         time.sleep(3)
 
-        #Get page source
+        # Get page source
         content = browser.page_source
         search_page = BeautifulSoup(content, "lxml")
 
         # Get course basic metadata
-        link_block = str(search_page.select('a[class*="browse-course-card--link--"]')[0])
-        self.link = 'https://udemy.com' + str(BeautifulSoup(link_block, 'lxml').find('a')['href'])
+        link_block = str(search_page.select(
+            'a[class*="browse-course-card--link--"]')[0])
+        self.link = 'https://udemy.com' + \
+            str(BeautifulSoup(link_block, 'lxml').find('a')['href'])
 
-        
         # Scrape Information on course_page
         url = self.link
         browser.get(url)
 
-        time.sleep(2) #Wait for the browser to completely load the page. You can change this depending on your internet speed.
+        # Wait for the browser to completely load the page. You can change this depending on your internet speed.
+        time.sleep(4)
 
         # Get the html
         content = browser.page_source
-        
-        #Parse HTML
+
+        # Parse HTML
         course_page = BeautifulSoup(content, "lxml")
 
-        no_of_buttons = len(course_page.find_all("button", attrs={'data-purpose':'show-more'}))
-        #check if the show more button for sections exists or not.
+        no_of_buttons = len(course_page.find_all(
+            "button", attrs={'data-purpose': 'show-more'}))
+        time.sleep(3)
+        # check if the show more button for sections exists or not.
         if no_of_buttons > 0:
-            browser.execute_script("""var element = document.querySelector('[data-purpose="show-more"]'); element.click();""")
-        browser.close()
-        #Get the title
-        self.title = course_page.find("h1", class_="udlite-heading-xl clp-lead__title clp-lead__title--small").text
+            browser.execute_script(
+                """var element = document.querySelector('[data-purpose="show-more"]'); element.click();""")
 
-        #Get the headline text. (Kind of like the subtitle which is usually displayed under the tite on the course page)
-        self.headline = course_page.find("div", attrs={'data-purpose':'lead-headline'}).text
+        # Get the html
+        content = browser.page_source
+        browser.close()
+        # Parse HTML
+        course_page = BeautifulSoup(content, "lxml")
+
+        # Get the title
+        self.title = course_page.find(
+            "h1", class_="udlite-heading-xl clp-lead__title clp-lead__title--small").text
+
+        # Get the headline text. (Kind of like the subtitle which is usually displayed under the tite on the course page)
+        self.headline = course_page.find(
+            "div", attrs={'data-purpose': 'lead-headline'}).text
 
         # Get the rating
-        self.rating = course_page.find("span", attrs={'data-purpose':'rating-number'}).text
+        self.rating = course_page.find(
+            "span", attrs={'data-purpose': 'rating-number'}).text
 
-        #Get number of ratings
-        self.no_of_ratings = course_page.find("div", class_="clp-lead__element-item clp-lead__element-item--row").find_all("span")[3].text.replace("(","").replace(" ratings)", "")
+        # Get number of ratings
+        self.no_of_ratings = course_page.find("div", class_="clp-lead__element-item clp-lead__element-item--row").find_all(
+            "span")[3].text.replace("(", "").replace(" ratings)", "")
 
-        #Get the number of students
-        self.student_enrolls = course_page.find("div", attrs={'data-purpose':'enrollment'}).text.replace(" students", "")
+        # Get the number of students
+        self.student_enrolls = course_page.find(
+            "div", attrs={'data-purpose': 'enrollment'}).text.replace(" students", "")
 
         # Get the instructors name
-        self.instructor = course_page.find("a", class_="udlite-btn udlite-btn-large udlite-btn-link udlite-heading-md udlite-text-sm udlite-instructor-links").find("span").text.replace("\n","")
+        self.instructor = course_page.find(
+            "a", class_="udlite-btn udlite-btn-large udlite-btn-link udlite-heading-md udlite-text-sm udlite-instructor-links").find("span").text.replace("\n", "")
 
-        # Get content information 
-        content_info = course_page.select('span[class*="curriculum--content-length-"]')[0].text.replace("\xa0", " ").split(" • ")
-        self.duration = content_info[2].replace(" total length","")
+        # Get content information
+        content_info = course_page.select(
+            'span[class*="curriculum--content-length-"]')[0].text.replace("\xa0", " ").split(" • ")
+        self.duration = content_info[2].replace(" total length", "")
 
-        self.no_of_lectures = content_info[1].replace(" lectures","")
-        
+        self.no_of_lectures = content_info[1].replace(" lectures", "")
+
         self.no_of_sections = content_info[0].replace(" sections", "")
         # Get breadcrumb tags
         self.tags = []
@@ -110,11 +132,14 @@ class UdemyCourse():
             self.tags.append(tag.text)
 
         # This is come noob code. I didn't know of any other way, so please, if you have a better alternative, create a PR :pray:
-        self.no_of_rating = course_page.find("span", class_="", string=lambda x: x and x.startswith('(')).text.replace("(", "").replace(" ratings)", "")
-        self.no_of_students = course_page.find("div", attrs={"data-purpose": "enrollment"}).text.replace("\n","").replace(" students", "")
-        self.course_language = course_page.find("div", class_="clp-lead__element-item clp-lead__locale").text.replace("\n","")
+        self.no_of_rating = course_page.find("span", class_="", string=lambda x: x and x.startswith(
+            '(')).text.replace("(", "").replace(" ratings)", "")
+        self.no_of_students = course_page.find("div", attrs={
+                                               "data-purpose": "enrollment"}).text.replace("\n", "").replace(" students", "")
+        self.course_language = course_page.find(
+            "div", class_="clp-lead__element-item clp-lead__locale").text.replace("\n", "")
 
-        #Get the pointers present in "What you'll learn part"
+        # Get the pointers present in "What you'll learn part"
         self.objectives = []
         for objective in course_page.find_all("span", class_="what-you-will-learn--objective-item--ECarc"):
             self.objectives.append(objective.text)
@@ -135,7 +160,7 @@ class UdemyCourse():
 
         # Get the long description section. Each paragraph is concatenated to the description string separated by a "\n" or a new line.
         self.description = ""
-        for parahraph in course_page.find("div", attrs={'data-purpose':'safely-set-inner-html:description:description'}).find_all("p"):
+        for parahraph in course_page.find("div", attrs={'data-purpose': 'safely-set-inner-html:description:description'}).find_all("p"):
             self.description += parahraph.text + "\n"
 
         # Get the information in the target section section
@@ -144,38 +169,40 @@ class UdemyCourse():
             self.target_audience.append(a.text)
 
         # Get the url of the course
-        self.banner = str(course_page.find("div", class_="intro-asset--asset--1eSsi").find("img").attrs['src']).replace("240x135","480x270")
+        self.banner = str(course_page.find(
+            "div", class_="intro-asset--asset--1eSsi").find("img").attrs['src']).replace("240x135", "480x270")
 
 
 def course_to_json(course):
-    #Initialize a new Sections array which will contain coverted dictionaries instead of objects for json serialization
+    # Initialize a new Sections array which will contain coverted dictionaries instead of objects for json serialization
     new_section_list = []
-
 
     for section in course.Sections:
         # Initialize a new lessons array which will contain coverted dictionaries instead of objects for every section
         new_lesson_list = []
 
         for lesson in section.lessons:
-            
+
             dict = lesson.__dict__         # Convert the lesson object to a dictionary
-            del dict['lesson_html']        # Remove the html code from the dictionary
+            # Remove the html code from the dictionary
+            del dict['lesson_html']
             new_lesson_list.append(dict)   # Add the dictionary to the new list
 
-        
-        section.lessons = new_lesson_list   #Update the lessons object array with the new lessons dictionary array for this section
-        section_dict = section.__dict__     #Convert the section into a dictionary
+        # Update the lessons object array with the new lessons dictionary array for this section
+        section.lessons = new_lesson_list
+        section_dict = section.__dict__  # Convert the section into a dictionary
 
         # Remove the html code from the dictionary
-        del section_dict['html']            
+        del section_dict['html']
         del section_dict['lesson_blocks']
 
         # Add the dictionary to the new list
         new_section_list.append(section_dict)
 
-    #Update the sections object array with the new sections dictionary array 
+    # Update the sections object array with the new sections dictionary array
     course.Sections = new_section_list
 
     # Dump the python object as a json in 'object.json' file. You can change this to whatever you want
     with open('object.json', 'w') as file:
-        file.write(json.dumps(course.__dict__)) #Convert the course to dictionary and dump.
+        # Convert the course to dictionary and dump.
+        file.write(json.dumps(course.__dict__))
