a9ce2ef77562c7c6df46e2de6ab8c7956d43f2f0
sortedcord <divyashgupta2005@gmail.com>
2021-08-15 10:08:12 +0530

    🔍Pass in query directly from terminal

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from udemyscraper import *
 import getopt
 import sys
 
-warn = True
 
 # Remove 1st argument from the
 # list of command line arguments
 argumentList = sys.argv[1:]
 
 # Options
-options = "hvn"
+options = "hvnq:"
 
 # Long options
-long_options = ["help", "version", "no-warn"]
+long_options = ["help", "version", "no-warn", "query"]
+
+# Tool Defaults
+warn = True
+search_query = ""
 
 try:
     # Parsing argument
@@ -34,11 +37,15 @@ try:
         elif currentArgument in ("-n", "--no-warn"):
             warn = False
 
+        elif currentArgument in ("-q", "--query"):
+            search_query = currentValue
+
 
 except getopt.error as err:
     # output error, and return with an error code
     print(str(err))
 
-
-course = UdemyCourse(input("Enter search query: "), warn)
+if search_query == "":
+    search_query = input("Enter the search query: ")
+course = UdemyCourse(search_query, warn)
 course.fetch_course()
