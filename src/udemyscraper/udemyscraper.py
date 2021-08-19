cb30f22da455075a390a50300cda0b758d08a975
sortedcord <divyashgupta2005@gmail.com>
2021-08-19 10:06:33 +0530

    Added Exception Handling if driver not found

--- src/udemyscraper/udemyscraper.py
+++ src/udemyscraper/udemyscraper.py
@@ -4,7 +4,7 @@ __starttime__ = time.time()
 # Selenium Libraries
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait  # to wait until page loads
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 from selenium import webdriver  # for webdriver
@@ -17,7 +17,7 @@ import getopt
 import sys
 from colorama import Fore, Style
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 
 def loginfo(message):
@@ -37,11 +37,53 @@ def quick_display(course):
 
 
 def display_warn():
-    with open('texts/warning.txt') as file:
-        lines = file.readlines()
-        for line in lines:
-            print(line.replace("\n", ""))
-            time.sleep(0.4)
+    print("""
+Thank You for using udemyscraper
+
+Before fetching the course information be sure that 
+you have google chrome >= 92.x.x installed on your machine. 
+Other chromium based browsers such as brave, edge, 
+opera, vivaldi, etc will not work.
+
+This tool is only meant to be used for educational purposes. 
+Please do not abuse it as rate limits might still apply.
+
+Happy Scraping!
+~ Sortedcord
+""")
+
+
+def display_help():
+    print(f"""
+udemyscraper {__version__} (cli)
+Usage: udemyscraper.py [options] command
+
+udemyscraper is a free and open source tool beautiful soup, 
+that fetches udemy course information. Get udemy course 
+information and convert it to json, csv or xml file, 
+without authentication.
+
+Most Used Commands:
+
+    -h  --help          Displays information about udemyscraper and its usage
+    -v  --version       Displays the version of the tool
+    -n  --no-warn       Disables the warning when initializing the udemyscourse class
+    -q  --query         You can pass the search query directly. If the search query
+                        is of multiple words then be sure to enclose the entire string
+                        in quotes.
+    -b  --browser       Allows you to select the browser you would like to use for Scraping
+                        Values: "chrome" or "firefox". Defaults to chrome if no argument is passed.
+    -l  --headless      Can be used to disable/enable suppressing of the browser. Can be set to `true`
+                        or `false`. Defaults to `true` if no argument is passed.
+    -d  --dump          Dump the course object to the specified format. Available formats current 
+                        include `json`
+    -o  --output        Output the course object to the specified format. Deafults to 'output.json` for 
+                        json.
+    -e  --debug         Enable Debug Logging
+        --quiet         Disables the logo and the intro when running the `main.py` file.
+    -t  --time          Displays the time taken for the entire script to run. Is enabled by default when
+                        quiet mode is disabled.   
+""")
 
 # Section class will contain an array with lesson classes
 
@@ -49,8 +91,8 @@ def display_warn():
 class Lesson():
     def __init__(self, lesson_html):
 
-        # Since the structure of previewable 
-        # lessons is different from that of the 
+        # Since the structure of previewable
+        # lessons is different from that of the
         # ones that are not:
 
         if "Preview" in lesson_html.select_one("div").text:
@@ -127,14 +169,21 @@ class UdemyCourse():
                 loginfo("Headless enabled")
             option.add_experimental_option(
                 'excludeSwitches', ['enable-logging'])
-            browser = webdriver.Chrome(chrome_options=option)
+            try:
+                browser = webdriver.Chrome(options=option)
+            except WebDriverException:
+                print("Chrome driver not found. Make sure it is in your path")
+                exit()
         elif self.Preferences['browser_preference'] == "FIREFOX":
             fireFoxOptions = webdriver.FirefoxOptions()
             if self.Preferences['headless'] == True:
                 loginfo("Headless enabled")
                 fireFoxOptions.set_headless()
-            browser = webdriver.Firefox(
-                firefox_options=fireFoxOptions)
+            try:
+                browser = webdriver.Firefox(firefox_options=fireFoxOptions)
+            except WebDriverException:
+                print("Geko driver not found. Make sure it is in your path")
+                exit()
 
         loginfo(
             "Redirecting to the searchpage")
@@ -408,16 +457,11 @@ if __name__ == "__main__":
 
             # Help argument
             if currentArgument in ("-h", "--help"):
-                with open('texts/help.txt') as file:
-                    lines = file.readlines()
-                    for line in lines:
-                        print(line.replace("\n", ""))
-                        time.sleep(0.1)
-                    exit()
+                display_help()
 
             # Version argument
             elif currentArgument in ("-v", "--version"):
-                print("Udemyscraper version 0.0.4", "\n")
+                print(f"udemyscraper {__version__} (cli)", "\n")
                 exit()
 
             # Disable warning
