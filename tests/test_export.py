49cdfea55fa14198f229260a53394c6856b7e481
Aditya Gupta <divyashgupta2005@gmail.com>
2021-08-31 14:31:24 +0530

    ⭐Added Testing for exports

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,3 +1,40 @@
 from udemyscraper import UdemyCourse
 from udemyscraper.export import export_course
 
+global formats
+formats = ['xml', 'json', 'csv']
+
+import os, shutil
+
+
+def main():
+    course = UdemyCourse({'cache':True, 'cache_dir':'.udscraper_cache_test'})
+    course.fetch_course("learn python")
+    return course
+
+
+def test_export_default():
+    course = main() #Get course
+
+    for format in formats:
+        if format == "csv":
+            course = [course]
+
+        export_course(course, formats)
+        assert os.path.isfile(f'course.{format}')
+        shutil.rmtree(f'course.{format}')
+
+
+def test_export_custom():
+    course = main() #Get course
+
+    for format in formats:
+        if format == "csv":
+            course = [course]
+
+        export_course(course, formats, f"mycustomexport.{format}")
+        assert os.path.isfile(f'course.{format}')
+        shutil.rmtree(f'course.{format}')
+
+
+
